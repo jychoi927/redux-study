0f1f4e6ae204fdc76c3e33506f03d02deecf2ab9
jychoi927 <jychoi927@gmail.com>
2020-07-20 17:10:55 +0900

    To do list with redux

--- src/index.js
+++ src/index.js
@@ -1,53 +1,73 @@
 import { createStore } from "redux";
 
-const add = document.getElementById("add");
-const minus = document.getElementById("minus");
-const number = document.querySelector("span");
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
 
-//action type을 변수로 관리
-//문자열보다 변수로 관리하는게 에러감지나 유지보수면에서 더 편하기 때문
-const ADD = "ADD"
-const MINUS = "MINUS"
+const ADD_TODO = "ADD_TODO";
+const DELETE_TODO = "DELETE_TODO";
 
-//Reducer : Store에 있는 state를 modify하는 역할. Reducer 이외에는 state를 modify하지 못함
-//Reducer가 return 하는 값이 해당 state가 됨
-//인자는 current state와 action 두 가지가 필요함
-//action은 Object type(객체)이어야 하고 type이라는 프로퍼티를 반드시 가져야 함
-const countModifier = (count = 0, action) => {
-  //Reducer는 switch문으로 관리
-  switch(action.type){
-    case ADD:
-      return count + 1;
-    case MINUS:
-      return count - 1;
-    default:
-      return count;
-  }
+const addToDo = text => {
+  return {
+    type: ADD_TODO,
+    text
+  };
+};
+const deleteToDo = id => {
+  return {
+    type: DELETE_TODO,
+    id
+  };
 };
 
-//Store : state(data)을 저장하는 곳
-//Store 생성시 reducer를 인자로 반드시 넣어줘야 함
-//Store는 4 가지 함수를 가짐
-//1) dispatch 2) subscribe 3) getState 4) replaceReducer
-const countStore = createStore(countModifier);
-
-const onChange = () => {
-  //getState은 현재 state을 반환
-  number.innerText = countStore.getState();
+const reducer = (state = [], action) => {
+  switch (action.type) {
+    case ADD_TODO:
+      const newToDoObj = { text: action.text, id: Date.now() };
+      return [newToDoObj, ...state];
+    //store에 있는 state는 절대 mutate 해서는 안됨
+    //todo list를 삭제하기 위해 state에서 삭제하려는 값을 빼버리는 방식(mutate)이 아닌
+    //삭제하려는 값을 제외한 새로운 값(여기서는 array)을 만들어서 return 해야 함
+    case DELETE_TODO:
+      return state.filter(toDo => toDo.id !== action.id);
+    default:
+      return state;
+  }
 }
 
-//Subscirbe : Store에 상태가 변경되면 인자로 넣은 함수를 실행
-countStore.subscribe(onChange);
+const store = createStore(reducer);
+store.subscribe(() => console.log(store.getState()));
 
-//dispatch : reducer와 communicate하는 함수
-//dispatch를 통해 reducer에 action을 보냄
-const handleAdd = () => {
-  countStore.dispatch({ type: ADD })
+const dispatchAddToDo = text => {
+  store.dispatch(addToDo(text));
+};
+const dispatchDeleteToDo = e => {
+  const id = parseInt(e.target.parentNode.id);
+  store.dispatch(deleteToDo(id));
 }
 
-const handleMius = () => {
-  countStore.dispatch({ type: MINUS })
+const paintToDos = () => {
+  const toDos = store.getState();
+  ul.innerHTML = "";
+  toDos.forEach(toDo => {
+    const li = document.createElement("li");
+    const btn = document.createElement("button");
+    btn.innerText = "DEL";
+    btn.addEventListener("click", dispatchDeleteToDo);
+    li.id = toDo.id;
+    li.innerText = toDo.text;
+    li.appendChild(btn);
+    ul.appendChild(li);
+  })
 }
 
-add.addEventListener("click", handleAdd)
-minus.addEventListener("click", handleMius)
+store.subscribe(paintToDos);
+
+const onSubmit = e => {
+  e.preventDefault();
+  const toDo = input.value;
+  input.value = "";
+  dispatchAddToDo(toDo)
+};
+
+form.addEventListener("submit", onSubmit);
