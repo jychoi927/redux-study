3b527850aa540b30793cce89d505563e8a98c7f1
jychoi927 <jychoi927@gmail.com>
2020-07-22 16:37:03 +0900

    To do list with redux toolkit

--- src/store.js
+++ src/store.js
@@ -1,33 +1,21 @@
-import { createStore } from 'redux';
+import { createAction, createReducer, configureStore } from '@reduxjs/toolkit';
 
-const ADD = "ADD"
-const DELETE = "DELETE"
+const addToDo = createAction("ADD");
+const deleteToDo = createAction("DELETE");
 
-const addToDo = text => {
-    return {
-        type: ADD,
-        text
-    }
-}
-const deleteToDo = id => {
-    return {
-        type: DELETE,
-        id: parseInt(id)
-    }
-}
+//createReducer를 사용하면 state를 mutate 할 수 있음
+//state를 mutate 할 경우 return을 하면 안됨 
+//mutate 하지 않는 경우는 새로운 state를 return
+const reducer = createReducer([], {
+    [addToDo]: (state, action) => {
+        state.push({ text: action.payload, id: Date.now() })
+    },
+    [deleteToDo]: (state, action) => 
+        state.filter(toDo => toDo.id !== action.payload)
+})
 
-const reducer = (state = [], action) => {
-    switch (action.type) {
-        case ADD:
-            return [{ text: action.text, id: Date.now() }, ...state];
-        case DELETE:
-            return state.filter(toDo => toDo.id !== action.id);
-        default:
-            return state;
-    }
-}
-
-const store = createStore(reducer);
+//configureStore는 크롬에서 Redux DevTools를 사용할 수 있게 해줌
+const store = configureStore({reducer});
 
 export const actionCreators = {
     addToDo,
